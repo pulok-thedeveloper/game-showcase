6a535ada2db398039835b20de82e96501e9f520a
pulok-thedeveloper <mdpulokhasan@gmail.com>
2024-05-20 17:31:33 +0600

    scroll to top

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 const Main = () => {
   return (
     <>
+      <ScrollRestoration />
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
